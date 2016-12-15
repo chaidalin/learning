a2e31ce44ffd399d2b9e2022e641a7bdfdda9e91
Li Chailin <chailin.li@outlook.com>
2016-12-15 20:03:03 +0800

    add auto-intended

--- py/Read txt and print json.py
+++ py/Read txt and print json.py
@@ -1,15 +1,15 @@
 import json
 
 
-def print_as_json(x):
+def print_as_json(x,n):
     for y in x:
         value = x.get(y)
-
         if isinstance(value, dict):
-            print_as_json(value)
+            n = n+1
+            print_as_json(value,n)
         else:
-            print(y, ":", value)
+            print("\t"*n, y, ":", value)
     return
 with open("userdata.txt") as f:
     jsonf = json.load(f)
-print_as_json(jsonf)
+print_as_json(jsonf,0)
