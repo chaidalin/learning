7d2273803fb24c81bc339ba94700832483fb0283
Li Chailin <chailin.li@outlook.com>
2016-12-16 19:49:24 +0800

    新建程序员借口大全 增加注释

--- py/Read txt and print json.py
+++ py/Read txt and print json.py
@@ -1,16 +1,17 @@
 import json
 
 
+# 从文本文档读取连续的json格式字符串转换成字典再逐个数据打印到屏幕，把dict类型的数据再按层级缩进再展开。
 def print_dict_as_json(x, n):
     for y in x:
         value = x.get(y)
         if isinstance(value, dict):
-            print("\t"*n, y, ":")
+            print("\t" * n, y, ":")
             n += 1
             print_dict_as_json(value, n)
             n -= 1
         else:
-            print("\t"*n, y, ":", value)
+            print("\t" * n, y, ":", value)
     return
 with open("userdata.txt") as f:
     jsonf = json.load(f)
